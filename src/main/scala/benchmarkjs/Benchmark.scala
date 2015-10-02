b597f2a320ef3a53da5a3bdc3a3a56d3dc8245ab
Sébastien Doeraene <sjrdoeraene@gmail.com>
2015-10-02 22:20:27 +0200

    Initial benchmark.

---
+++ src/main/scala/benchmarkjs/Benchmark.scala
@@ -0,0 +1,32 @@
+package benchmarkjs
+
+import scala.scalajs.js
+import js.annotation._
+
+@js.native
+@JSName("Benchmark.Suite")
+class Suite(name: String = ???) extends js.Object {
+  def add(name: String, fn: js.Function0[_]): this.type = js.native
+
+  def on(tpe: String, listener: js.Function0[_]): this.type = js.native
+  def on(tpe: String, listener: js.Function1[js.Dynamic, _]): this.type = js.native
+
+  def run(options: Suite.RunOptions = ???): Unit = js.native
+}
+
+object Suite {
+  @ScalaJSDefined
+  trait RunOptions extends js.Object {
+    def async: Boolean
+    def queued: Boolean
+  }
+
+  object RunOptions {
+    def apply(async: Boolean = true, queued: Boolean = true): RunOptions = {
+      js.Dynamic.literal(
+          async = async,
+          queued = queued
+      ).asInstanceOf[RunOptions]
+    }
+  }
+}
--- src/main/scala/longbenchmark/LongBenchmark.scala
+++ src/main/scala/longbenchmark/LongBenchmark.scala
@@ -4,9 +4,35 @@ import scala.scalajs.js
 import org.scalajs.dom
 
 object LongBenchmark extends js.JSApp {
+  final val SampleCount = 1000
+
+  val longs1 = Array.fill(SampleCount)(scala.util.Random.nextLong())
+  val longs2 = Array.fill(SampleCount)(scala.util.Random.nextLong())
+
+  val results = new Array[Long](SampleCount)
+
   def main(): Unit = {
-    val paragraph = dom.document.createElement("p")
-    paragraph.innerHTML = "<strong>It works!</strong>"
-    dom.document.getElementById("playground").appendChild(paragraph)
+    val suite = new benchmarkjs.Suite
+
+    val long1 = longs1(0)
+    val long2 = longs2(0)
+
+    suite
+      .add("+", { () =>
+        long1 + long2
+      })
+      .add("-", { () =>
+        long1 - long2
+      })
+      .add("*", { () =>
+        long1 * long2
+      })
+      .add("/", { () =>
+        long1 / long2
+      })
+      .on("cycle", { (event: js.Dynamic) =>
+        println(event.target.toString())
+      })
+      .run(benchmarkjs.Suite.RunOptions())
   }
 }
