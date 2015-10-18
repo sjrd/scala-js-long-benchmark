b9d4c1a51cafbdede663d7d32f4978b4eaf0bffd
Damien Engels <engelsdamien@gmail.com>
2015-10-18 14:22:45 +0200

    Seems to work, need to run benchmark in async so we can see results

--- src/main/scala/longbenchmark/ScalameterLongBenchmark.scala
+++ src/main/scala/longbenchmark/ScalameterLongBenchmark.scala
@@ -12,130 +12,179 @@ import org.scalameter.CurveData
  * @author dengels
  */
 object ScalameterLongBenchmark extends js.JSApp {
-   
+
   object LongBench extends Bench.OfflineReport {
-  
-    val gwtLong = Gen.single("GoogleWebkit")(gwtlike.Build.fromLong _)
-    val optgwtLong = Gen.single("OptGoogleWebkit")(optgwtlike.Build.fromLong _)
-    val teavmLong = Gen.single("TeaVM")(teavmlike.Build.fromLong _)
-    val optteavmLong = Gen.single("OptTeaVM")(optteavmlike.Build.fromLong _)
-    
-    performance of "RuntimeLong" in {
+
+    //val gwtLong = Gen.single("gwt")(gwtlike.Build.fromLong _)
+    //val optgwtLong = Gen.single("optgwt")(optgwtlike.Build.fromLong _)
+    //val teavmLong = Gen.single("teavm")(teavmlike.Build.fromLong _)
+    //val optteavmLong = Gen.single("optteavm")(optteavmlike.Build.fromLong _)
+
+    val sample: Gen[Seq[(Long, Long)]] = Gen.unit("sample1").map {
+      _ =>
+        for (
+          i <- 1L to 1000L;
+          j <- (Long.MaxValue - 1000) to Long.MaxValue
+        ) yield (i, j)
+    }
+
+    // Google Web Toolkit
+    performance of "gwt" in {
+      measure method "plus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              gwtlike.Build.fromLong(a) + gwtlike.Build.fromLong(b)
+          }
+        }
+      }
+
+      measure method "minus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              gwtlike.Build.fromLong(a) - gwtlike.Build.fromLong(b)
+          }
+        }
+      }
       
-       val numbers = for (
-            i <- 1L to 1000L;
-            j <- (Long.MaxValue - 1000) to Long.MaxValue) yield (i, j)
+      measure method "times" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              gwtlike.Build.fromLong(a) * gwtlike.Build.fromLong(b)
+          }
+        }
+      }
       
-      measure method "+" in {
-            
-        using(gwtLong) in { longBuilder =>
+      measure method "div" in {
+        using(sample) in { numbers =>
           numbers.foreach {
-            case (a, b) => longBuilder(a) + longBuilder(b)
-          }
-        }
-        
-        using(optgwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) + longBuilder(b)
-          }
-        }
-        
-        using(teavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) + longBuilder(b)
-          }
-        }
-        
-        using(optteavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) + longBuilder(b)
-          }
-        }
-      }
-       
-       measure method "-" in {
-            
-        using(gwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) - longBuilder(b)
-          }
-        }
-        
-        using(optgwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) - longBuilder(b)
-          }
-        }
-        
-        using(teavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) - longBuilder(b)
-          }
-        }
-        
-        using(optteavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) - longBuilder(b)
-          }
-        }
-      }
-       
-       measure method "*" in {
-            
-        using(gwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) * longBuilder(b)
-          }
-        }
-        
-        using(optgwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) * longBuilder(b)
-          }
-        }
-        
-        using(teavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) * longBuilder(b)
-          }
-        }
-        
-        using(optteavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) * longBuilder(b)
-          }
-        }
-      }
-       
-       measure method "/" in {
-            
-        using(gwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) / longBuilder(b)
-          }
-        }
-        
-        using(optgwtLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) / longBuilder(b)
-          }
-        }
-        
-        /*using(teavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) / longBuilder(b)
-          }
-        }*/
-        
-        using(optteavmLong) in { longBuilder =>
-          numbers.foreach {
-            case (a, b) => longBuilder(a) / longBuilder(b)
+            case (a, b) =>
+              gwtlike.Build.fromLong(a) / gwtlike.Build.fromLong(b)
           }
         }
       }
     }
     
-  override def reporter = new Reporter[Double] {
+    // Optimized Google Web Toolkit
+    performance of "optgwt" in {
+      measure method "plus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optgwtlike.Build.fromLong(a) + optgwtlike.Build.fromLong(b)
+          }
+        }
+      }
+
+      measure method "minus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optgwtlike.Build.fromLong(a) - optgwtlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      measure method "times" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optgwtlike.Build.fromLong(a) * optgwtlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      measure method "div" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optgwtlike.Build.fromLong(a) / optgwtlike.Build.fromLong(b)
+          }
+        }
+      }
+    }
+    
+    // Tea VM
+    performance of "teavm" in {
+      measure method "plus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              teavmlike.Build.fromLong(a) + teavmlike.Build.fromLong(b)
+          }
+        }
+      }
+
+      measure method "minus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              teavmlike.Build.fromLong(a) - teavmlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      measure method "times" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              teavmlike.Build.fromLong(a) * teavmlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      /*measure method "div" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              teavm.Build.fromLong(a) / teavm.Build.fromLong(b)
+          }
+        }
+      }*/
+    }
+    
+    // Optimized Tea VM
+    performance of "optteavm" in {
+      measure method "plus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optteavmlike.Build.fromLong(a) + optteavmlike.Build.fromLong(b)
+          }
+        }
+      }
+
+      measure method "minus" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optteavmlike.Build.fromLong(a) - optteavmlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      measure method "times" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optteavmlike.Build.fromLong(a) * optteavmlike.Build.fromLong(b)
+          }
+        }
+      }
+      
+      measure method "div" in {
+        using(sample) in { numbers =>
+          numbers.foreach {
+            case (a, b) =>
+              optteavmlike.Build.fromLong(a) / optteavmlike.Build.fromLong(b)
+          }
+        }
+      }
+    }
+
+    override def reporter = new Reporter[Double] {
       def report(result: CurveData[Double], persistor: Persistor): Unit = {
         println(s"\nOne Test finished : success = ${result.success}")
 
@@ -149,11 +198,18 @@ object ScalameterLongBenchmark extends js.JSApp {
 
         println("\nMeasurements:")
         val measurements = result.measurements
-        measurements.foreach { x => 
+        measurements.foreach { x =>
           val axisData = x.params.toString
           val result = x.value + x.units
           println(s"$axisData : $result")
-          }
+        }
+        
+        val measure = math.round(measurements.head.value * 100) / 100
+        val units = measurements.head.units
+        
+        val id = ctx.scopeList.mkString("-")
+        println(s"id = $id")
+        dom.document.getElementById(id).innerHTML = s"$measure $units"
       }
 
       def report(results: Tree[CurveData[Double]], persistor: Persistor): Boolean = {
@@ -167,10 +223,8 @@ object ScalameterLongBenchmark extends js.JSApp {
     override def executor = LocalExecutor(warmer, aggregator, measurer)
     override def persistor = Persistor.None
   }
-  
-  
-  
-  def main : Unit = {
-     LongBench.main(Array())
-   } 
+
+  def main: Unit = {
+    LongBench.main(Array())
+  }
 }
